fd3dd5715adb51314507ac34a62e4a34d879e46b
Chris Cardelle <chris.cardelle@gmail.com>
2019-03-23 15:57:20 -0400

    Added backwards command, and collision object.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,8 @@
+//Christian Cardelle
+//IronHack
+//Mars Rover Project 
+//March 2019
+
 // Rover Object Goes Here
 // ======================
 
@@ -50,33 +55,109 @@ function turnRight(rover){
   }
   console.log("turnRight was called!");
 }
-// Function that moves the rover by one space on the grid, depending on direction.
+// Function that moves the rover by one space on the grid, depending on direction. Boundaries set to ignore command outside 10x10 array.
 function moveForward(rover){
+  if (obstacleCheck()){
+    return;
+  }
   switch(rover.direction){
     case "N":
+    if ( rover.y == 0){
+      console.log("Northern Boundary reached. Please turn around.");
+      break;
+    }
+  
+    else {
     rover.y = rover.y - 1;    
     break;
-
+    }
     case "E":
+    if ( rover.x == 10){
+      console.log("Eastern Boundary reached. Please turn around.");
+      break;
+    }
+    else {
     rover.x = rover.x + 1;
     break;
-
+    }
     case "S":
+    if ( rover.y == 10){
+      console.log("Southern Boundary reached. Please turn around.");
+      break;
+    }
+    else {
     rover.y = rover.y + 1;
     break;
-
+    }
     case "W":
+    if ( rover.y == 0){
+      console.log("Western Boundary reached. Please turn around.");
+      break;
+    }
+    else {
     rover.x = rover.x - 1;
     break;
-  
-  }
+    
+    }
 
   console.log("moveForward was called")
 }
+}
 
-// Function that receives a string as an arg and calls the correct command function depending on the letter. (r,l,f)
+
+// Function that moves the rover backwards by one space on the grid, depending on direction. Boundaries set to ignore command outside 10x10 array.
+function moveBackward(rover){
+  if (obstacleCheck()){
+    return;
+  }
+  switch(rover.direction){
+    case "N":
+    if ( rover.y == 10){
+      console.log("Southern Boundary reached. Please turn around.");
+      break;
+    }
+  
+    else {
+    rover.y = rover.y + 1;    
+    break;
+    }
+    case "E":
+    if ( rover.x == 0){
+      console.log("Western Boundary reached. Please turn around.");
+      break;
+    }
+    else {
+    rover.x = rover.x - 1;
+    break;
+    }
+    case "S":
+    if ( rover.y == 0){
+      console.log("Northern Boundary reached. Please turn around.");
+      break;
+    }
+    else {
+    rover.y = rover.y - 1;
+    break;
+    }
+    case "W":
+    if ( rover.y == 10){
+      console.log("Eastern Boundary reached. Please turn around.");
+      break;
+    }
+    else {
+    rover.x = rover.x + 1;
+    break;
+    
+    }
+
+  console.log("moveBackward was called")
+}
+}
+
+// Function that receives a string as an arg and calls the correct move function depending on the letter. (r,l,f)
 function rover_Commands(commands){
 var xy = 0;
+rover.travelLog.clear; 
 for (i = 0; i < commands.length; i++)
   if (commands[i] == "r"){
     turnRight(rover);
@@ -90,10 +171,35 @@ for (i = 0; i < commands.length; i++)
     moveForward(rover);
     rover.travelLog[xy] = rover.x + "," + rover.y;
     xy++;
-  } else {
-    console.log("Can only send r, l, and f as commands")
+
+  } else if (commands[i] == "b") {
+    moveBackward(rover);
+    rover.travelLog[xy] = rover.x + "," + rover.y;
+    xy++;
+    
+  }  else {
+    console.log("Can only send r, l, and f as commands, command ignored.")
   }
   console.log(rover.travelLog);  
 }
   
-  
+//Function checks for collision at coordinates 5,5.  
+function obstacleCheck() {
+  if (rover.x == 5){
+    if (rover.y++ == 5 || rover.y-- == 5){
+    console.log("Crashed into a rock at coordinates 5,5. Refresh to try again.");
+     return(true);
+    } else if (rover.y == 5){
+      if (rover.x++ == 5 || rover.x-- == 5){
+        console.log("Crashed into a rock at coordinates 5,5. Refresh to try again.");
+        return(true);
+    }
+  }
+    
+
+  } 
+
+  else {
+    return(false);
+  }
+}
